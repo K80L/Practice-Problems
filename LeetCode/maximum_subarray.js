8c0c1d2f254af8ff3719add1332117500823dfe0
kevly <93ly.kevin@gmail.com>
2019-12-11 00:44:01 -0800

    Work on problems

--- LeetCode/maximum_subarray.js
+++ LeetCode/maximum_subarray.js
@@ -10,3 +10,30 @@
 
 // # If you have figured out the O(n) solution,
 // try coding another solution using the divide and conquer approach, which is more subtle.
+
+var maxSubArray = function(nums) {
+  let arr = [nums[0]];
+  // [-2,1,-2,4,3,5,6,1,5]
+  for (let i = 1; i < nums.length; i++) {
+    let temp = Math.max(nums[i], nums[i] + arr[i - 1]);
+    arr.push(temp);
+  }
+
+  return Math.max(...arr);
+};
+
+function maxSubArr(arr) {
+  let maxes = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (maxes[i - 1] + arr[i] > maxes[i - 1]) {
+      maxes.push(maxes[i - 1] + arr[i]);
+    } else if (arr[i] > maxes[i - 1] + arr[i]) {
+      maxes.push(arr[i]);
+    } else {
+      maxes.push(0);
+    }
+  }
+}
+
+// maxes: [-2, 1, 0, 4, ]
